38430e190872d4c24a4190ecaccdfd5933cbf67d
Anatoly Frolov <anatolyfroloff@gmail.com>
2025-11-25 14:30:49 +1000

    refactor(image-viewer): remove image viewer
    Image Viewer debug view has become redundant due to presenting HTTP Image Server

--- src/main/java/ru/anafro/hyperstream/leonardo/Main.java
+++ src/main/java/ru/anafro/hyperstream/leonardo/Main.java
@@ -3,8 +3,7 @@ package ru.anafro.hyperstream.leonardo;
 import ru.anafro.hyperstream.leonardo.configuration.ApplicationEnvironment;
 import ru.anafro.hyperstream.leonardo.configuration.Configuration;
 import ru.anafro.hyperstream.leonardo.configuration.exceptions.ConfigurationLoadingException;
-import ru.anafro.hyperstream.leonardo.debug.ImageViewer;
-import ru.anafro.hyperstream.leonardo.debug.exceptions.ImageServerRunException;
+import ru.anafro.hyperstream.leonardo.http.exceptions.ImageServerRunException;
 import ru.anafro.hyperstream.leonardo.facades.HyperstreamLeonardo;
 import ru.anafro.hyperstream.leonardo.http.ImageServer;
 
@@ -21,9 +20,6 @@ public class Main {
         final var image = leonardo.generateImage();
 
         if (configuration.isDebug()) {
-            final var viewer = new ImageViewer();
-            viewer.show(image);
-
             final var server = new ImageServer(leonardo);
             server.run();
             Thread.currentThread().join();
