a934cc6e5f6eca20bba295d4b6bf108ac265d274
Anatoly Frolov (anafro) <anatolyfroloff@gmail.com>
2026-01-10 15:24:28 +1200

    refactor!: make generators accept id and size
    BREAKING CHANGE: Now profile picture generators must be created with
    image generation size, and used with pfp id. All generators using random
    number generation in pfp drawing, must utilize id as randomizing seed.

--- src/main/java/ru/anafro/hyperstream/leonardo/generators/LargeMagellanicCloudProfileImageGenerator.java
+++ src/main/java/ru/anafro/hyperstream/leonardo/generators/LargeMagellanicCloudProfileImageGenerator.java
@@ -10,62 +10,60 @@ import ru.anafro.hyperstream.leonardo.utils.math.Points;
 import java.awt.*;
 import java.util.Random;
 
-public class LargeMagellanicCloudProfileImageGenerator extends AbstractProfilePictureGenerator {
-    private final int seed;
-
-    public LargeMagellanicCloudProfileImageGenerator(String seed) {
-        this.seed = seed.hashCode();
+public class LargeMagellanicCloudProfileImageGenerator extends ProfilePictureGenerator {
+    public LargeMagellanicCloudProfileImageGenerator(Dimension size) {
+        super(size);
     }
 
     @Override
-    public void draw(Dimension size, int[] canvas) {
+    public void draw(Dimension size, int[] canvas, int userId) {
         final var gradients = new int[][] {
-                {0xff0061ff, 0xff60efff},
-                {0xff42047e, 0xff07f49e},
-                {0xffe9d022, 0xffe60b09},
-                {0xff2feaa8, 0xff028cf3},
-                {0xfff40752, 0xfff9ab8f},
-                {0xff3d05dd, 0xfffcb0f3},
-                {0xffef476f, 0xffffd166},
-                {0xffffd166, 0xff06d6a0},
-                {0xff06d6a0, 0xff118ab2},
-                {0xff118ab2, 0xff073b4c},
-                {0xff073b4c, 0xffef476f},
-                {0xffffd60a, 0xff003566},
-                {0xffe5383b, 0xffd3d3d3},
-                {0xffccff33, 0xff004b23},
-                {0xfffff0f3, 0xffc9184a},
-                {0xffff595e, 0xffffca3a},
-                {0xffffca3a, 0xff1982c4},
-                {0xff00cecb, 0xffffffea},
-                {0xffff5e5b, 0xff00cecb},
-                {0xfff72585, 0xff4cc9f0},
-                {0xfff4e409, 0xff710000},
-                {0xff98da00, 0xffff8a1b},
-                {0xff1fd224, 0xffff5c14},
-                {0xff8ede02, 0xff0000fe},
-                {0xff00ffe7, 0xfffd5200},
-                {0xff004dff, 0xff4dff00},
-                {0xffff228c, 0xffffd800},
-                {0xffffd800, 0xff22b1ff},
-                {0xff22b1ff, 0xffff228c},
-                {0xff03045e, 0xffb8fb3c},
-                {0xff080708, 0xff3772ff},
-                {0xff33658a, 0xff86bbd8},
-                {0xffef745c, 0xff34073d},
-                {0xffdb2763, 0xffb0db43},
-                {0xff292f36, 0xff4ecdc4},
-                {0xffffd800, 0xff7902aa},
-                {0xffff0000, 0xffffffff},
-                {0xff272727, 0xffd4aa7d},
-                {0xffffe74c, 0xffff5964},
-                {0xffff6b35, 0xfff7c59f},
-                {0xff2a9d8f, 0xffe76f51},
-                {0xff71a5de, 0xfff8f9fb},
-                {0xff1efc1e, 0xff424342},
+                { 0xff0061ff, 0xff60efff },
+                { 0xff42047e, 0xff07f49e },
+                { 0xffe9d022, 0xffe60b09 },
+                { 0xff2feaa8, 0xff028cf3 },
+                { 0xfff40752, 0xfff9ab8f },
+                { 0xff3d05dd, 0xfffcb0f3 },
+                { 0xffef476f, 0xffffd166 },
+                { 0xffffd166, 0xff06d6a0 },
+                { 0xff06d6a0, 0xff118ab2 },
+                { 0xff118ab2, 0xff073b4c },
+                { 0xff073b4c, 0xffef476f },
+                { 0xffffd60a, 0xff003566 },
+                { 0xffe5383b, 0xffd3d3d3 },
+                { 0xffccff33, 0xff004b23 },
+                { 0xfffff0f3, 0xffc9184a },
+                { 0xffff595e, 0xffffca3a },
+                { 0xffffca3a, 0xff1982c4 },
+                { 0xff00cecb, 0xffffffea },
+                { 0xffff5e5b, 0xff00cecb },
+                { 0xfff72585, 0xff4cc9f0 },
+                { 0xfff4e409, 0xff710000 },
+                { 0xff98da00, 0xffff8a1b },
+                { 0xff1fd224, 0xffff5c14 },
+                { 0xff8ede02, 0xff0000fe },
+                { 0xff00ffe7, 0xfffd5200 },
+                { 0xff004dff, 0xff4dff00 },
+                { 0xffff228c, 0xffffd800 },
+                { 0xffffd800, 0xff22b1ff },
+                { 0xff22b1ff, 0xffff228c },
+                { 0xff03045e, 0xffb8fb3c },
+                { 0xff080708, 0xff3772ff },
+                { 0xff33658a, 0xff86bbd8 },
+                { 0xffef745c, 0xff34073d },
+                { 0xffdb2763, 0xffb0db43 },
+                { 0xff292f36, 0xff4ecdc4 },
+                { 0xffffd800, 0xff7902aa },
+                { 0xffff0000, 0xffffffff },
+                { 0xff272727, 0xffd4aa7d },
+                { 0xffffe74c, 0xffff5964 },
+                { 0xffff6b35, 0xfff7c59f },
+                { 0xff2a9d8f, 0xffe76f51 },
+                { 0xff71a5de, 0xfff8f9fb },
+                { 0xff1efc1e, 0xff424342 },
         };
 
-        final var random = new Random(seed);
+        final var random = new Random(userId);
         final var ancientMagic = 0.001;
         final var speed = 2.250 + ancientMagic;
         final var grain = 0.080 + ancientMagic;
@@ -87,7 +85,8 @@ public class LargeMagellanicCloudProfileImageGenerator extends AbstractProfilePi
                 final var i = FlatCoordinates.of(xi, yi, size.width);
                 final var x = Factor.of(xi, size.width) + ancientMagic;
                 final var y = Factor.of(yi, size.width) + ancientMagic;
-                final var starDensity = 1 / Math.pow(minStarSquareEdge * MathFunctions.map(Math.sin((x + y) * galacticArms), -Math.PI, +Math.PI, 1. - galacticLushness, 1), 2);
+                final var starDensity = 1 / Math.pow(minStarSquareEdge * MathFunctions
+                        .map(Math.sin((x + y) * galacticArms), -Math.PI, +Math.PI, 1. - galacticLushness, 1), 2);
 
                 var rx = Points.rotateX(x, y, cx, cy, alpha);
                 var ry = Points.rotateY(x, y, cx, cy, alpha);
@@ -101,7 +100,9 @@ public class LargeMagellanicCloudProfileImageGenerator extends AbstractProfilePi
                 rx += ry / 42 / 42;
                 ry = Math.pow(ry, 0.42);
 
-                double starLuminosity = Math.pow(random.nextDouble() < starDensity ? random.nextDouble() * maxStarLuminosity : 0., supermassiveBlackHoleDistance);
+                double starLuminosity = Math.pow(
+                        random.nextDouble() < starDensity ? random.nextDouble() * maxStarLuminosity : 0.,
+                        supermassiveBlackHoleDistance);
                 double gradientFactor = Factor.shaker(speed * ((rx + ry) + (1 + random.nextDouble() * grain)), bezier);
 
                 canvas[i] = Colors.mix(gradient[0], gradient[1], gradientFactor);
--- src/main/java/ru/anafro/hyperstream/leonardo/generators/ProfilePictureGeneratorFactory.java
+++ src/main/java/ru/anafro/hyperstream/leonardo/generators/ProfilePictureGeneratorFactory.java
@@ -1,11 +1,23 @@
 package ru.anafro.hyperstream.leonardo.generators;
 
+import java.awt.Dimension;
 import java.util.UUID;
 
-public final class ProfilePictureGeneratorFactory {
-    private ProfilePictureGeneratorFactory() { /* util class */ }
+import ru.anafro.hyperstream.leonardo.utils.sugar.Guard;
 
-    public static AbstractProfilePictureGenerator newDefaultGenerator() {
-        return new LargeMagellanicCloudProfileImageGenerator(UUID.randomUUID().toString());
+public final class ProfilePictureGeneratorFactory {
+    private ProfilePictureGeneratorFactory() {
+        // util class
+    }
+
+    public static ProfilePictureGenerator create(String generatorName, int width, int height) {
+        Guard.deny(width <= 0, "Width cannot be negative.");
+        Guard.deny(height <= 0, "Width cannot be negative.");
+        final Dimension size = new Dimension(width, height);
+
+        return switch (generatorName.toLowerCase()) {
+            case "lmc", "large-magellanic-cloud" -> new LargeMagellanicCloudProfileImageGenerator(size);
+            default -> throw new IllegalArgumentException("No generator named '{}'.".formatted(generatorName));
+        };
     }
 }
